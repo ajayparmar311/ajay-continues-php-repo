3772a0f0169321d40576fc39f6dadbcb56c07eda
ajayparmar311 <ajayp.parmar311@gmail.com>
2024-03-30 17:03:06 +0530

    changed index.php file

--- php/index.php
+++ php/index.php
@@ -1,3 +1,3 @@
 <html>
-  <?php echo "Hello world from a php container" ?>
+  <?php echo "Hello world from a php container i am ajay parmar from india and testing gitcommit and image building" ?>
 </html>
